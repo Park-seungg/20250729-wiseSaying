61ee78efadb8562e939439c5acfc52bcacdda112
Park-seungg <parkis1101@gmail.com>
2025-07-31 11:33:49 +0900

    Rq 클래스 구현 (1)

---
+++ src/main/java/com/ll/Rq.java
@@ -0,0 +1,49 @@
+package com.ll;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Rq {
+    private final String actionName;
+    // final -> 최초 선언 후 재할당 불가
+    private final Map<String, String> paramsMap;
+    //목록
+    // ?
+    // searchKeywordType=content
+    // &
+    // searchKeyword=자바
+
+    public Rq(String cmd) {
+        paramsMap = new HashMap<>();
+
+        String[] cmdBits = cmd.split("\\?", 2);
+        actionName = cmdBits[0];
+        String queryString = cmdBits.length > 1 ? cmdBits[1].trim() : "";
+
+        String[] queryStringBits = queryString.split("&");
+
+        for (String queryParam : queryStringBits) {
+            String[] queryParamBits = queryParam.split("=", 2);
+            String key = queryParamBits[0].trim();
+            String value = queryParamBits.length > 1 ? queryParamBits[1].trim() : "";
+
+            if (value.isEmpty()) {
+                continue;
+            }
+
+            paramsMap.put(key, value);
+        }
+    }
+
+    public String getActionName() {
+        return actionName;
+    }
+
+    public String getParam(String paramName, String defaultValue) {
+        if (paramsMap.containsKey(paramName)) {
+            return paramsMap.get(paramName);
+        } else {
+            return defaultValue;
+        }
+    }
+}
